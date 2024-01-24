2861801e0d2cf75a27ab017a61f0db3a8696fee6
Pankaj Kumar <pankaj.ky3007@gmail.com>
2024-01-25 00:31:35 +0530

    all file updated

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+function App() {
+  return (
+    <>
+      <div className="text-center">
+        <h1 className="text-3xl mt-10">Welcome to tailwind react starter pack</h1>
+      </div>
+      
+    </>
+  );
+}
+
+export default App;
